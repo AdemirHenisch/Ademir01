621bf98eff10153bbd94149ab1bcbfc9956bbc07
AdemirHenisch <140716181+AdemirHenisch@users.noreply.github.com>
2025-03-25 14:39:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ aumentaFonteBotao.addEventListener('click', function(){;
         document.body.style.fontSize=`${tamanhoAtualFonte}rem`
     })
 diminuiFonteBotao.addEventListener('click', function(){;
-        tamanhoAtualFonte -=0.1;
+        tamanhoAtualFonte += -0.1;
         document.body.style.fontSize=`${tamanhoAtualFonte}rem`
     })
 
