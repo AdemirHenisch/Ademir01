07d8bf937e7398b3fadfa78cc26ffa3f76f9433a
AdemirHenisch <140716181+AdemirHenisch@users.noreply.github.com>
2025-03-25 14:20:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 })
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+})
+document.addEventListener('DOMContentLoaded', function () {
+    let tamanhoAtualFonte = 1;
+const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 diminuiFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += -0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
