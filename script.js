f0b64788ddef586f1ed3619d2e7a71b4a1328b81
AdemirHenisch <140716181+AdemirHenisch@users.noreply.github.com>
2025-04-01 14:18:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ diminuiFonteBotao.addEventListener('click', function(){;
         tamanhoAtualFonte += -0.1;
         document.body.style.fontSize=`${tamanhoAtualFonte}rem`
     })
+})
 
+document.addEventListener('DOMContentLoaded', function () {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
+    botaoDeAcessibilidade.addEventListener('click', function () {
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
+    //Código fonte seguinte ocultado no slide
 })
